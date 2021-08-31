d3f09db4e55479867de3190d6a0ca9a1d9025a5a
ZZIDZZ <zidanutomo@gmail.com>
2021-08-31 09:44:31 +0700

    added environment variable to store token outside source code

--- client.py
+++ client.py
@@ -1,21 +1,19 @@
 #%%
 from pyppeteer import launch
+from types import SimpleNamespace
 import discord
 import asyncio
 import json
-from types import SimpleNamespace
-from dotenv import load_dotenv
 import os
 #import time
-
 from discord.ext import commands
-
+from dotenv import load_dotenv
 # command prefix s-
 client = commands.Bot(command_prefix="s-")
 
 # environment variable
+load_dotenv('.env')
 TOKEN = os.getenv('TOKEN')
-
 
 # event
 @client.event # bot online (saat .py ini dijalankan)
@@ -368,3 +366,5 @@ client.run(TOKEN)
 
 # on repl.it if not working :
 # install-pkg gconf-service libasound2 libatk1.0-0 libatk-bridge2.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+
+# %%
