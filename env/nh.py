203e438a5380b44c7dd32e4ebed8b360c8b11128
ZZIDZZ <zidanutomo@gmail.com>
2021-09-19 12:14:43 +0700

    fix

---
+++ env/nh.py
@@ -0,0 +1,353 @@
+from __future__ import annotations
+import discord, os
+from types import SimpleNamespace
+import json
+from pyppeteer import launch
+import asyncio
+
+class StoredCodes:
+  def __init__(self, tag, freq, codes):
+    self.tag = tag
+    self.freq = freq
+    self.codes = codes
+
+class NhSession:
+  def __init__(self, tag: str, freq: str, amount: int, sessionName: str, channel: str, server: str) -> None:
+   
+    self.tag = tag
+    self.freq = freq
+    self.amount = amount
+    self.sessionName = sessionName
+    self.running = True
+    self.channel = channel
+    self.server = server
+    self.savedCodes = []
+    self.lastCodes = []
+
+
+try:
+  os.mkdir('server')
+except:
+  pass
+
+
+async def stopSeering(client, ctx, sessionName):
+  """ this function will stop a session given by its sessionName
+  """
+  # read json
+  channel = discord.utils.get(client.get_all_channels(), name=str(ctx.channel))
+  with open(f"server/{ctx.guild.id}/nhSessions.json", "r+") as f:
+    content = f.read()
+    nhSessions = json.loads(content, object_hook= lambda o: SimpleNamespace(**o))
+    for nhSession in nhSessions:
+      await ctx.send(nhSession.__dict__)
+      if nhSession.sessionName is sessionName:
+        # ketemu
+        nhSession.running = False
+        break
+    else:
+      await ctx.send(f"Tidak ada nhSession dengan nama {sessionName}")
+    f.seek(0)
+    f.write(nhSessions)
+    f.truncate()
+    f.close()
+
+      
+  # check if exist session with $sessionName
+
+  # else show all session within the channel invoked
+
+async def seerNH_here(client, ctx, *args):
+  """ mengaktifkan nhSession 
+  """
+  # get channel where seerNH_here invoked
+  channel = discord.utils.get(client.get_all_channels(), name=str(ctx.channel))
+  with open(f"server/{ctx.guild.id}/nhSessions.json", "r+") as f:
+    content = f.read()
+    nhSessions = json.loads(content, object_hook= lambda o: SimpleNamespace(**o))
+    for nhSession in nhSessions:
+      await ctx.send(nhSession.__dict__)
+      if nhSession.sessionName is sessionName:
+        # ketemu
+        await ctx.send(f"nhSession dengan nama {sessionName}, sudah dijalankan")
+    else:
+      await ctx.send(f"Tidak ada nhSession dengan nama {sessionName}")
+    f.seek(0)
+    f.write(nhSessions)
+    f.truncate()
+    f.close()
+  await NHPLoop(channel, args)
+
+async def NHPLoop(channel, args): # get 5 codes of popular art on main page
+  print(args)
+  
+  # lowering the args
+  args = [arg.lower() for arg in args]
+  
+  # process args
+  # --tag
+  # --frequency
+  # --amount    amount is like grab top # but can be in recent too
+  additionalSelector = "" # this is for main page
+  if len(args) is 0:
+    additionalSelector = ".index-popular"
+    tag = "main"
+    freq = ""
+    amount = 5
+  
+  helpList = [arg for arg in args if "--help" in arg]
+  print(helpList)
+  if len(helpList) is 1:
+    await channel.send("melakukan scrap di website kesayangan(nh)\n\nformat cmd: s-seerNH_here --tag=optional --freq=optional --amount=optional --sessionName\nuntuk freq hanya bisa recent, today, week, all-time\nuntuk amount untuk main page 1-5, selain itu 1-25\npastikan tag benar ada! kalau ada spasi ganti dengan \"-\"")
+    return
+
+  # input tag
+  tagList = [arg for arg in args if "--tag=" in arg]
+  print(tagList)
+  if len(tagList) is 1:
+    tag = tagList[0][tagList[0].index("=") + 1 :]
+  else:
+    tag = "main"
+  print(f"tag = {tag}")
+    
+  # input freq
+  # valid input for freq
+  # recent
+  # today
+  # week
+  # all-time
+  # freqList, list is in its name but actually its just element that contains --freq=something
+  freqList = [arg for arg in args if "--freq=" in arg]
+  print(freqList)
+  if len(freqList) is 1 and "recent" not in freqList[0]:
+    freq = freqList[0][freqList[0].index("=") + 1:]
+    prefix = "popular"
+    if freq is "all-time": # all-time = https://nhentai.net/tag/{yourtag}/popular
+      freq = prefix
+    else:
+      freq = f"{prefix}-{freq}"
+  else:
+    freq = ""
+  print(f"freq = {freq}")
+  
+  # input amount
+  # valid input 1 - 25 if not main page else just 1 - 5
+  # not support for multiple pages
+  amountList = [arg for arg in args if "--amount=" in arg]
+  if len(amountList) is 1:
+    amount = int(amountList[0][amountList[0].index("=") + 1:])
+    if amount < 1:
+      await channel.send("ngajak berantem?")
+      return
+  else:
+    amount = 5
+  print(f"amount = {amount}")
+  
+  # session name
+  # valib input no spaces
+  sessionNameList = [arg for arg in args if "--sessionName=" in arg]
+  if len(sessionNameList) is 1:
+    sessionName = sessionNameList[0][sessionNameList[0].index("=") + 1:]
+  else:
+    sessionName = f"{channel.name}_{tag}_{freq}_{amount}"
+    
+
+
+
+  # check and create session if exist return
+  f = open(f"/server/{channel.guild.id}/nhcode.json", "r+")
+  content = f.read()
+  nhSessions = json.loads(content, object_hook= lambda o: SimpleNamespace(**o))
+
+  for nhSession in nhSessions:
+    if nhSession.sessionName is sessionName:
+      await channel.send(f"There is conflict session name.\nSession with name '{sessionName}' is existed, try to use other name... TERMINATING")
+      info = "```py\nSession with same name:\n\n"
+      info += f"server      = {nhSession.server}\n"
+      info += f"channel     = {nhSession.channel}\n"
+      info += f"tag         = {nhSession.tag}\n"
+      info += f"freq        = {nhSession.freq}\n"
+      info += f"amount      = {nhSession.amount}"
+      info += f"sessionName = {nhSession.sessionName}\n```"
+      await channel.send(info)
+      f.close()
+      return
+  else:
+    await channel.send("sessionName check: OK")
+    newNhSession = NhSession(tag, freq, amount, sessionName, channel.name, channel.guild.name)
+    nhSessions.append(newNhSession)
+    nhSessions = json.dumps(nhSessions.__dict__)
+    await channel.send(nhSessions)
+    f.seek(0)
+    f.write(nhSessions)
+    f.truncate()
+    f.close()
+
+  # lulus check informasi seering
+  info = "```py\nseering Info:\n\n"
+  info += f"server      = {channel.guild.name}\n"
+  info += f"channel     = {channel.name}\n"
+  info += f"tag         = {tag}\n"
+  info += f"freq        = {freq}\n"
+  info += f"amount      = {amount}"
+  info += f"sessionName = {sessionName}\n```"
+  await channel.send(info)
+
+  nhSessionRunning = True
+
+  subjectLink = "https://nhentai.net"
+  if tag is not "main":
+    subjectLink +=  f"/tag/{tag}/{freq}"
+  
+  while nhSessionRunning:
+    # savedCodes structures
+    # [
+    #  {
+    #   "tag": "some-tag1",
+    #   "freq": "freq-types2",
+    #   "codes": [...,...,...]
+    #  },{
+    #   "tag": "some-tag2",
+    #   "freq": "freq-types2"
+    #   "codes": [...,...,...]
+    #  }
+    # ]
+
+    f = open(f"/server/{channel.guild.id}/nhcode.json", "r+")
+    content = f.read()
+    print(f"content = {content}")
+    nhSessions = json.loads(content, object_hook= lambda o: SimpleNamespace(**o))
+
+    # debug
+    try:
+      await channel.send(f"nhSessions = {nhSessions}")
+    except Exception as e:
+      await channel.send(e)
+
+    # debug 
+    try:
+      for nhSession in nhSessions:
+        if nhSession.sessionName is sessionName:
+          if not nhSession.running:
+            return
+          subjectNhSession = nhSession
+          break
+    except:
+      pass
+    # subjectNhSession -> NhSession Object
+
+    
+
+    try:
+      [codes, captions] = await nhScraper(subjectLink, additionalSelector, amount)
+    except Exception as e:
+      await channel.send(f"some error has occurred\nError message:{e}")
+      return
+
+    if len(lastCodes) == 0: # first time run on certain tag and freq
+      for i in range(0, len(codes)):
+        # https://stackoverflow.com/questions/64527464/clickable-link-inside-message-discord-py
+        [tags, thumbnail, pages] = await getTagsAndThumbnailAndPagesFromCode(codes[i])
+        embed = discord.Embed()
+        if "http" not in thumbnail:
+          await channel.send(f"can't load the thumbnail, thumbnail fed: {thumbnail}")
+        else:
+          embed.set_image(url=thumbnail)
+        embed.description = f"{captions[i]}\n\nTags: •{' •'.join(tags)}\nPages: {pages}\n\n[#{codes[i]}](https://nhentai.net/g/{codes[i]})."
+        await channel.send(embed=embed)
+      lastCodes = codes
+      newStoredCodes = StoredCodes(tag, freq, codes)
+      savedCodes.append(newStoredCodes)
+
+    else: # in loop
+      adaBeda = False
+      codeBeda = []
+      for i in range(0, len(codes)):
+        if codes[i] in lastCodes:
+          pass
+        else:
+          codeBeda.append(codes[i])
+          adaBeda = True
+          [tags, thumbnail, pages] = await getTagsAndThumbnailAndPagesFromCode(codes[i])
+          embed = discord.Embed()
+          print(thumbnail)
+          print("\n")
+          if "http" not in thumbnail:
+            await channel.send(f"can't load the thumbnail, thumbnail fed: {thumbnail}")
+          else:
+            embed.set_image(url=thumbnail)
+          embed.description = f"{captions[i]}\n\nTags: •{' •'.join(tags)}\nPages: {pages}\n\n[#{codes[i]}](https://nhentai.net/g/{codes[i]})."
+          await channel.send(embed=embed)
+      if not adaBeda:
+        await channel.send("no new art in popular(main page)")
+      else:
+        for savedCode in savedCodes:
+          if savedCode.tag == tag and savedCode.freq == freq:
+            for code in codeBeda:
+              savedCode.codes.append(code)
+            break
+        lastCodes = codes
+    savedCodes = json.dumps([savedCode.__dict__ for savedCode in savedCodes])
+    print(savedCodes)
+    f.seek(0)
+    f.write(savedCodes)
+    f.truncate()
+    f.close()
+    await asyncio.sleep(3600)
+
+
+# this is the scrap function 
+async def nhScraper(subjectLink: str, additionalSelector: str, amount: int) -> list[list[str], list[str]]:
+  """this is the scrap function """
+  browser = await launch(ignoreHTTPSErrors = True, headless = True, args=["--no-sandbox"])
+  page = await browser.newPage()
+  await page.goto(subjectLink)
+
+  # ambil tag div popular
+  subjectDiv = await page.querySelector(f".container.index-container{additionalSelector}")
+
+  # buat list yang isinya anchor dari karya popular
+  subjectAnchor = await subjectDiv.querySelectorAll(".cover")
+  codes = []
+  #thumbnails = []
+  captions = []
+  if amount > len(subjectAnchor):
+    amount = len(subjectAnchor)
+  # olah setiap anchor untuk di ekstrak kode dan thumbnailnya
+  for i in range(0, amount):
+    code = await page.evaluate('(ele) => ele.getAttribute("href")', subjectAnchor[i])
+    code = code[3:-1]
+
+    #thumbnailURL = await page.evaluate('(ele) => ele.querySelector("img").getAttribute("src")', subjectAnchor[i])
+    caption = await page.evaluate('(ele) => ele.querySelector("div").innerText', subjectAnchor[i])
+
+    #print(thumbnailURL)
+    print(code)
+    codes.append(code)
+    #thumbnails.append(thumbnailURL)
+    captions.append(caption)
+    
+  await browser.close()
+  return [codes, captions]
+
+
+async def getTagsAndThumbnailAndPagesFromCode(code: str = "177013") -> list[list[str], str, str]:
+  """return tags, thumbail url and pages count given code """
+  browser = await launch(ignoreHTTPSErrors = True, headless = True, args=["--no-sandbox"])
+  page = await browser.newPage()
+  await page.goto(f"https://nhentai.net/g/{code}/")
+  spanTag = await page.querySelectorAll("span.tags")
+  spanTag = spanTag[2]
+  anchorTags = await spanTag.querySelectorAll("a")
+  tags = []
+  for a in anchorTags:
+    tag = await page.evaluate('(ele) => ele.querySelector("span.name").innerText', a)
+    tags.append(tag)
+  imgThumbnail = await page.querySelector(".lazyload")
+  thumbnailURL = await page.evaluate("""(ele) => ele.getAttribute("data-src")""", imgThumbnail)
+  anchorPages = await page.querySelectorAll("a.tag")
+  anchorPages = anchorPages[-1]
+  pages =  await page.evaluate(""" (ele) => ele.querySelector("span.name").innerText """, anchorPages)
+  await page.close()
+  await browser.close()
+  return [tags, thumbnailURL, pages]
---
+++ env/player.py
@@ -0,0 +1,323 @@
+from __future__ import annotations
+import asyncio, discord, datetime, youtube_dl, threading, json
+from posixpath import pardir
+# from discord.enums import Theme
+from youtubesearchpython import VideosSearch
+import os
+from types import SimpleNamespace
+
+class ServerQueue:
+  def __init__(self,serverID):
+    self.serverID = serverID
+    self.queueLen = 0
+    self.curQueue = 0
+    self.songQueue = []
+  def checkQueueResidue(self):
+    dirMsg = ""
+    if not os.path.exists(f'server/{self.serverID}'):
+      os.makedirs(f'server/{self.serverID}')
+      dirMsg += f"server {self.serverID} made"
+    try:
+      os.remove(f'server/{self.serverID}/music{self.curQueue}')
+      dirMsg += f"removed at {self.curQueue}"
+    except:
+      dirMsg += "no queue residue found\n"
+    return dirMsg
+  
+class Song:
+  def __init__(self, metaInfo, link, queueNum):
+    self.metaInfo = metaInfo
+    self.link = link
+
+    pass
+try:
+  os.mkdir('server')
+except:
+  pass
+serverQueues = []
+
+async def play(client, ctx, *arg):
+  # grab the user who sent the command
+  user=ctx.message.author
+  voice_channel=user.voice.channel
+  
+  # kalau bukan link
+  print(arg)
+  if not "youtu" in arg:
+    arg = " ".join(arg)
+    arg = searchVideoByName(arg)
+
+    arg = arg.result()["result"][0]["link"]
+
+    # await ctx.send(arg)
+    
+  # only play music if user is in a voice channel
+  if voice_channel is not None:
+    try:
+      # https://discordpy.readthedocs.io/en/stable/api.html#discord.VoiceClient
+    
+      
+      # WHAT THE FUCK WITH THIS MESSY CODE
+      channel = ctx.message.author.voice.channel
+      voice_client = discord.utils.get(client.voice_clients, guild=ctx.guild) 
+      try:
+        serverID = f"{ctx.message.guild.id}" 
+      except Exception as e:
+        await ctx.send(e)
+      
+      # jadi fungsi bawah ini tu return 1 klo di list serverqueues ada yg mengandung serverID tertentu, return none klo gaada
+      ###
+      for serverQueue in serverQueues:
+        await ctx.send(serverQueue)
+      try:
+        if next((1 for serverQueue in serverQueues if serverID in serverQueue.serverID), None) is None: 
+          serverQueues.append(ServerQueue(serverID))
+        serverReference = findServerByIDTryFix(serverID) # ni terlalu padet euy
+      except Exception as e:
+        await ctx.send(f"FINDSERVERBYIDMU MASALAH MAS\n{e}")
+      dirMsg = serverReference.checkQueueResidue()
+      await ctx.send(dirMsg)
+      # PLIS REFERENCE BUKAN COPY WKWKWKWKWKWKWWKWKWK masalahnya kalo list reference
+
+      #lah ini ngapa ke run ndiri?
+      # serverQueue = findServerByID(serverID) # ni terlalu padet euy 
+      
+      await ctx.send(serverReference)
+      await ctx.send(f"server id: {serverReference.serverID}")
+      await ctx.send(f"voice_client = {voice_client}")
+      if not voice_client is None: #test if voice channel exists
+        await ctx.send(f"connecting to {ctx.message.author.voice.channel} voice channel")
+        if not voice_client.is_connected():
+          voice_client = await channel.connect()
+      else:
+        try:
+          voice_client = await channel.connect()
+        except Exception as e:
+          await ctx.send(e)
+      
+      # download
+      await ctx.send(f"downloading: <{arg}>")
+      await ctx.send(f"voice_client = {voice_client}")
+      # await ctx.send(f"type of serverIndex = {type(serverIndex)}")
+      # try:
+      
+      # except Exception as e:
+      #   await ctx.send(f"ASU\n{e}")
+      #   for i in range(len(serverQueues)):
+      #     await ctx.send(f"{i}. {serverQueues[i]}")
+      #     # await ctx.send(serverIndex)
+      #     await ctx.send(serverQueues)
+      
+      
+      #aowkoawkaokaokwo pake server index ku
+      # oh si index cuman buat nyari servernya? ampe buat method find server by id wkkwk 
+      #hmmm
+      # create download thread and start it
+
+      # ini kayaknya cok emang serverQueues  DICt I think not, just a pure class trykk
+      downloadThread = DownloadThread(f"threadXXXgaming-{serverReference.curQueue}", arg, serverID)
+      downloadThread.start()
+
+      # create wait var for time memory
+      waiting = 0
+
+      # wait until downloadThread has attribute / property 'meta' i.e. downloadThread.meta
+      while not hasattr(downloadThread, "meta"):# https://stackoverflow.com/questions/843277/how-do-i-check-if-a-variable-exists
+        if waiting%5==0:
+          await ctx.send(f"waiting thread {downloadThread.name} to finish, {waiting} seconds passed")
+        await asyncio.sleep(1)
+        waiting += 1
+        if waiting > 20: # dengerin dosen dulu ,setuju
+          # terminate thread
+          await ctx.send("reaching time limit, terminating...")
+          return  #bisa to? kek di c kan jadinya blm dicoba sih, gatau error ato ga wkkwkwk
+      
+      # get meta from downloadThread
+      meta = downloadThread.meta
+      # creating song object
+      
+      song = Song(meta, arg, serverReference.queueLen)
+      serverReference.songQueue.append(song)
+      queueLen = len(serverReference.songQueue) # songQUeueu harusnya co nes eue EUE
+      serverReference.queueLen = queueLen
+
+      await ctx.send("finished downloading 👍")
+      # wait for player stop
+      if voice_client.is_playing():
+        while voice_client.is_playing():
+          await asyncio.sleep(1)
+
+      # create StreamPlayer
+      # Note: stupid solution but at least it worked
+      # if not user.voice.is_connected():
+      voice_client.play(discord.FFmpegPCMAudio(f'server/{serverID}/music{serverReference.curQueue}.mp3'), after=lambda e: print("done", e))
+      serverReference.curQueue+=1
+      sendPlaying = False
+      while voice_client.is_playing():
+        if not sendPlaying:
+          await ctx.send(f"Playing: {song.metaInfo['title']}\nUploader: {song.metaInfo['uploader']}\nDuration: {str(datetime.timedelta(seconds=song.metaInfo['duration']))}")
+          sendPlaying = True
+        # 👍 
+        await asyncio.sleep(1)
+      # disconnect after the player has finished
+      voice_client.stop()
+      
+    except Exception as e:
+      await ctx.send(e)
+  else:
+      await ctx.send('User is not in a channel.')
+      await ctx.send(ctx.message.author.voice.channel)
+
+class DownloadThread(threading.Thread):
+  def __init__(self, name, link, serverid):
+    threading.Thread.__init__(self)
+    self.name = name
+    self.link = link
+    self.serverid = serverid
+    # self.serverIndex = serverIndex
+  
+  def run(self) -> None:
+    print(f"starting thread {self.name}")
+    self.downloadmp3()
+    print(f"exiting thread {self.name}")
+
+  def downloadmp3(self) -> dict:
+    subjectServerQueue = findServerByIDTryFix(self.serverid) # ini butuh ServerQueue Object oalah
+    ydl_opts = {  
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }], #kek gini? # iyep padahal cuman buat ambil properti queuelen su ngambilnya object cuman sekali doang lagi akwokawokawokawoa slow but works noo comment try?yyyyyy
+        'outtmpl':f'server/{self.serverid}/music{subjectServerQueue.queueLen}.mp3',
+        'external-downloader': 'aria2c',
+        #'external-downloader-args': '-x 2',
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+      meta = ydl.extract_info(self.link)
+      ydl.download([self.link])
+      #downloadThread = threading.Thread(target=ydl.download, args=([link]), name=f"threadXXXgaming - {meta['title']}")
+      self.meta =  meta
+# def downloadmp3(link: str, serverid: str, serverIndex: int) -> list[object, dict]:
+#   ydl_opts = {
+#       'format': 'bestaudio/best',
+#       'postprocessors': [{
+#           'key': 'FFmpegExtractAudio',
+#           'preferredcodec': 'mp3',
+#           'preferredquality': '192',
+#       }],
+#       'outtmpl':f'server/{serverid}/music{serverQueues[serverIndex].queueLen}.mp3',
+#   }
+#   serverQueues[serverIndex].queueLen +=1
+#   with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#     meta = ydl.extract_info(link) # 
+#     downloadThread = threading.Thread(target=ydl.download, args=([link]), name=f"threadXXXgaming - {meta['title']}")
+#     return downloadThread, meta
+
+# In Progress
+async def addSongToQueue(arg):
+  return 
+
+async def clearQueue(ctx):
+  serverReference = findServerByIDTryFix(ctx.message.guild.id)
+  songCleared = 0
+  msg = ""
+  # try:
+  #   msg += f"active threads : {threading.active_count()}\n"
+  #   for thread in threading.enumerate():
+  #     msg += f"{thread.name}, alive: {thread.is_alive()}\n"
+  # except Exception as e:
+  #   msg += e
+  try:
+    for songNum in range(0,len(serverReference.songQueue)):
+      #check if the song is playing or the list is empty
+      if(serverReference.curQueue != songNum): 
+        try:
+          os.remove(f"server/{serverReference.serverID}/music{songNum}.mp3")
+          songCleared+=1
+        except Exception as e:
+          await ctx.send(f"error message : {e}\nqueue number {songNum} failed to be erased,\n")
+    serverReference.songQueue = []
+    serverReference.curQueue=0
+    serverReference.queueLen = len(serverReference.songQueue)
+    msg+=f"{songCleared} songs removed from queue\n"
+  except Exception as e:
+    msg+="clear queue failed\n" + e
+    pass
+  await ctx.send(msg)
+
+
+async def queue(ctx):
+  serverReference = findServerByIDTryFix(ctx.message.guild.id)
+  try:
+    for song in serverReference.songQueue:
+      await ctx.send(f"{song}. {song.metaInfo['title']}")
+  except:
+    await ctx.send("No song in queue")
+  await ctx.send(f"curQueue: {serverReference.curQueue}\nqueueLen: {len(serverReference.songQueue)}")
+  return
+
+
+async def voice_status(ctx, client):
+  print(client.voice_clients)
+  await ctx.send(type(client.voice_clients))
+  await ctx.send(client.voice_clients)
+  channel = discord.utils.get(ctx.guild.voice_channels, name=ctx.message.author.voice.channel)
+  voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+  if not voice is None: #test if voice is 
+    try:
+      b = vars(voice)
+      await ctx.send(', '.join("%s: %s" % item for item in b.items()))
+    except Exception as e:
+      await ctx.send(e)
+    await ctx.send(f"connecting{ctx.message.author.voice.channel}")
+    if not voice.is_connected():
+      await channel.connect()
+  else:
+    await channel.connect()
+      
+def getInfoYoutube(linkYoutubeOrSongName):
+  """mengembalikan informasi video\n
+  url\n
+  judul\n
+  durasi
+  """
+
+
+
+async def rmFromQueue(ctx):
+  index = int(ctx.message.content)
+  serverid = ctx.message.guild.id
+  serverIndex = findServerByID(serverid)
+  songName = serverQueues[serverIndex].songQueue[index].metainfo['title']
+  serverQueues[serverIndex].queueLen = len(serverQueues[serverIndex].songQueue)
+  try:
+    os.remove(f'server/{serverid}/music{serverQueues[serverIndex].curQueue}')
+    serverQueues[serverIndex].songQueue.pop(index)
+  except:
+    await ctx.send("can't remove currently playing music")
+    return
+  await ctx.send(f"removed {songName}")
+  return
+  
+def searchVideoByName(namaLagu):
+  """ VideoSearch Object
+  """
+  videosSearch = VideosSearch(namaLagu, limit = 2)
+  return videosSearch
+
+def getThreadCount():
+  return threading.active_count(), threading.current_thread()
+
+def findServerByID(serverid):
+  serverIndex = None
+  for i in range(0, len(serverQueues)):
+    if serverid in serverQueues[i].serverID:
+      serverIndex = i
+  return serverIndex
+
+def findServerByIDTryFix(serverid: str) -> ServerQueue:
+  for serverQueue in serverQueues:
+    if serverid in serverQueue.serverID:
+      return serverQueue
