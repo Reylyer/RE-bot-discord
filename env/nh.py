c6b4bd2a6232c0562e01a91b7d9f9e73bf6d19af
Reylyer <62227252+Reylyer@users.noreply.github.com>
2021-10-29 11:18:01 +0700

    Update nh.py

--- env/nh.py
+++ env/nh.py
@@ -1,56 +1,56 @@
 from __future__ import annotations
-import discord, os
+import discord, os, sys
 from types import SimpleNamespace
 import json
 from pyppeteer import launch
 import asyncio
 
 class StoredCodes:
-  def __init__(self, tag, freq, codes):
-    self.tag = tag
-    self.freq = freq
-    self.codes = codes
+    def __init__(self, tag, freq, codes):
+        self.tag = tag
+        self.freq = freq
+        self.codes = codes
 
 class NhSession:
-  def __init__(self, tag: str, freq: str, amount: int, sessionName: str, channel: str, server: str) -> None:
-   
-    self.tag = tag
-    self.freq = freq
-    self.amount = amount
-    self.sessionName = sessionName
-    self.running = True
-    self.channel = channel
-    self.server = server
-    self.savedCodes = []
-    self.lastCodes = []
+    def __init__(self, tag: str, freq: str, amount: int, sessionName: str, channel: str, server: str) -> None:
+    
+        self.tag = tag
+        self.freq = freq
+        self.amount = amount
+        self.sessionName = sessionName
+        self.running = True
+        self.channel = channel
+        self.server = server
+        self.savedCodes = []
+        self.lastCodes = []
 
 
 try:
-  os.mkdir('server')
+    os.mkdir('server')
 except:
-  pass
+    pass
 
 
 async def stopSeering(client, ctx, sessionName):
-  """ this function will stop a session given by its sessionName
-  """
-  # read json
-  channel = discord.utils.get(client.get_all_channels(), name=str(ctx.channel))
-  with open(f"server/{ctx.guild.id}/nhSessions.json", "r+") as f:
-    content = f.read()
-    nhSessions = json.loads(content, object_hook= lambda o: SimpleNamespace(**o))
-    for nhSession in nhSessions:
-      await ctx.send(nhSession.__dict__)
-      if nhSession.sessionName is sessionName:
-        # ketemu
-        nhSession.running = False
-        break
-    else:
-      await ctx.send(f"Tidak ada nhSession dengan nama {sessionName}")
-    f.seek(0)
-    f.write(nhSessions)
-    f.truncate()
-    f.close()
+    """ this function will stop a session given by its sessionName
+    """
+    # read json
+    channel = discord.utils.get(client.get_all_channels(), name=str(ctx.channel))
+    with open(f"server/{ctx.guild.id}/nhSessions.json", "r+") as f:
+        content = f.read()
+        nhSessions = json.loads(content, object_hook= lambda o: SimpleNamespace(**o))
+        for nhSession in nhSessions:
+    #      await ctx.send(nhSession.__dict__)
+            if nhSession.sessionName is sessionName:
+                # ketemu
+                nhSession.running = False
+                break
+        else:
+            await ctx.send(f"Tidak ada nhSession dengan nama {sessionName}")
+        f.seek(0)
+        f.write(nhSessions)
+        f.truncate()
+        f.close()
 
       
   # check if exist session with $sessionName
@@ -58,28 +58,42 @@ async def stopSeering(client, ctx, sessionName):
   # else show all session within the channel invoked
 
 async def seerNH_here(client, ctx, *args):
-  """ mengaktifkan nhSession 
-  """
-  # get channel where seerNH_here invoked
-  channel = discord.utils.get(client.get_all_channels(), name=str(ctx.channel))
-  with open(f"server/{ctx.guild.id}/nhSessions.json", "r+") as f:
-    content = f.read()
-    nhSessions = json.loads(content, object_hook= lambda o: SimpleNamespace(**o))
-    for nhSession in nhSessions:
-      await ctx.send(nhSession.__dict__)
-      if nhSession.sessionName is sessionName:
-        # ketemu
-        await ctx.send(f"nhSession dengan nama {sessionName}, sudah dijalankan")
+    """ mengaktifkan nhSession 
+    """
+    # get channel where seerNH_here invoked
+    channel = discord.utils.get(client.get_all_channels(), name=str(ctx.channel))
+
+    sessionNameList = [arg for arg in args if "--sessionName=" in arg]
+    print(sessionNameList)
+    sys.stdout.flush()
+    if len(sessionNameList) is 1:
+        sessionName = sessionNameList[0][sessionNameList[0].index("=") + 1 :]
     else:
-      await ctx.send(f"Tidak ada nhSession dengan nama {sessionName}")
-    f.seek(0)
-    f.write(nhSessions)
-    f.truncate()
-    f.close()
-  await NHPLoop(channel, args)
+        sessionName = ""
+    print(f"sessionName = {sessionName}")
+    sys.stdout.flush()
+
+    try:
+            os.mkdir(f"./servers/{ctx.message.guild.id}")
+    except:
+        pass
+        
+    with open(f"servers/{ctx.guild.id}/nhSessions.json", "r+") as f:
+        content = f.read()
+        nhSessions = json.loads(content, object_hook= lambda o: SimpleNamespace(**o))
+        for nhSession in nhSessions:
+    #     await ctx.send(nhSession.__dict__)
+            if nhSession.sessionName is sessionName:
+            # ketemu
+                await ctx.send(f"nhSession dengan nama {sessionName}, sudah dijalankan")
+        else:
+            await ctx.send(f"Tidak ada nhSession dengan nama {sessionName}")
+
+    await NHPLoop(channel, args)
 
 async def NHPLoop(channel, args): # get 5 codes of popular art on main page
   print(args)
+  sys.stdout.flush()
   
   # lowering the args
   args = [arg.lower() for arg in args]
@@ -97,6 +111,7 @@ async def NHPLoop(channel, args): # get 5 codes of popular art on main page
   
   helpList = [arg for arg in args if "--help" in arg]
   print(helpList)
+  sys.stdout.flush()
   if len(helpList) is 1:
     await channel.send("melakukan scrap di website kesayangan(nh)\n\nformat cmd: s-seerNH_here --tag=optional --freq=optional --amount=optional --sessionName\nuntuk freq hanya bisa recent, today, week, all-time\nuntuk amount untuk main page 1-5, selain itu 1-25\npastikan tag benar ada! kalau ada spasi ganti dengan \"-\"")
     return
@@ -104,11 +119,13 @@ async def NHPLoop(channel, args): # get 5 codes of popular art on main page
   # input tag
   tagList = [arg for arg in args if "--tag=" in arg]
   print(tagList)
+  sys.stdout.flush()
   if len(tagList) is 1:
     tag = tagList[0][tagList[0].index("=") + 1 :]
   else:
     tag = "main"
   print(f"tag = {tag}")
+  sys.stdout.flush()
     
   # input freq
   # valid input for freq
@@ -119,6 +136,7 @@ async def NHPLoop(channel, args): # get 5 codes of popular art on main page
   # freqList, list is in its name but actually its just element that contains --freq=something
   freqList = [arg for arg in args if "--freq=" in arg]
   print(freqList)
+  sys.stdout.flush()
   if len(freqList) is 1 and "recent" not in freqList[0]:
     freq = freqList[0][freqList[0].index("=") + 1:]
     prefix = "popular"
@@ -129,6 +147,7 @@ async def NHPLoop(channel, args): # get 5 codes of popular art on main page
   else:
     freq = ""
   print(f"freq = {freq}")
+  sys.stdout.flush()
   
   # input amount
   # valid input 1 - 25 if not main page else just 1 - 5
@@ -142,6 +161,7 @@ async def NHPLoop(channel, args): # get 5 codes of popular art on main page
   else:
     amount = 5
   print(f"amount = {amount}")
+  sys.stdout.flush()
   
   # session name
   # valib input no spaces
@@ -150,15 +170,16 @@ async def NHPLoop(channel, args): # get 5 codes of popular art on main page
     sessionName = sessionNameList[0][sessionNameList[0].index("=") + 1:]
   else:
     sessionName = f"{channel.name}_{tag}_{freq}_{amount}"
+  print(f"session name = {sessionName}")
+  sys.stdout.flush()
     
 
 
 
   # check and create session if exist return
-  f = open(f"/server/{channel.guild.id}/nhcode.json", "r+")
+  f = open(f"servers/{channel.guild.id}/nhSessions.json", "r+")
   content = f.read()
   nhSessions = json.loads(content, object_hook= lambda o: SimpleNamespace(**o))
-
   for nhSession in nhSessions:
     if nhSession.sessionName is sessionName:
       await channel.send(f"There is conflict session name.\nSession with name '{sessionName}' is existed, try to use other name... TERMINATING")
@@ -176,7 +197,8 @@ async def NHPLoop(channel, args): # get 5 codes of popular art on main page
     await channel.send("sessionName check: OK")
     newNhSession = NhSession(tag, freq, amount, sessionName, channel.name, channel.guild.name)
     nhSessions.append(newNhSession)
-    nhSessions = json.dumps(nhSessions.__dict__)
+    nhSessions = json.dumps([nhSession.__dict__ for nhSession in nhSessions], indent=4)
+    # ahasiswas = json.dumps([mahasiswa.__dict__ for mahasiswa in mahasiswas])
     await channel.send(nhSessions)
     f.seek(0)
     f.write(nhSessions)
@@ -189,7 +211,7 @@ async def NHPLoop(channel, args): # get 5 codes of popular art on main page
   info += f"channel     = {channel.name}\n"
   info += f"tag         = {tag}\n"
   info += f"freq        = {freq}\n"
-  info += f"amount      = {amount}"
+  info += f"amount      = {amount}\n"
   info += f"sessionName = {sessionName}\n```"
   await channel.send(info)
 
@@ -200,100 +222,82 @@ async def NHPLoop(channel, args): # get 5 codes of popular art on main page
     subjectLink +=  f"/tag/{tag}/{freq}"
   
   while nhSessionRunning:
-    # savedCodes structures
-    # [
-    #  {
-    #   "tag": "some-tag1",
-    #   "freq": "freq-types2",
-    #   "codes": [...,...,...]
-    #  },{
-    #   "tag": "some-tag2",
-    #   "freq": "freq-types2"
-    #   "codes": [...,...,...]
-    #  }
-    # ]
 
-    f = open(f"/server/{channel.guild.id}/nhcode.json", "r+")
+    f = open(f"servers/{channel.guild.id}/nhSessions.json", "r+")
     content = f.read()
     print(f"content = {content}")
+    sys.stdout.flush()
     nhSessions = json.loads(content, object_hook= lambda o: SimpleNamespace(**o))
 
-    # debug
-    try:
-      await channel.send(f"nhSessions = {nhSessions}")
-    except Exception as e:
-      await channel.send(e)
 
-    # debug 
+ #   await channel.send(nhSessions)
     try:
       for nhSession in nhSessions:
-        if nhSession.sessionName is sessionName:
-          if not nhSession.running:
-            return
+  #      await channel.send(f"{nhSession.sessionName} == {sessionName}")
+        print(f"{nhSession.sessionName} == {sessionName}")
+        sys.stdout.flush()
+        if nhSession.sessionName == sessionName:
           subjectNhSession = nhSession
           break
     except:
       pass
-    # subjectNhSession -> NhSession Object
-
-    
 
     try:
       [codes, captions] = await nhScraper(subjectLink, additionalSelector, amount)
     except Exception as e:
       await channel.send(f"some error has occurred\nError message:{e}")
       return
+    
 
-    if len(lastCodes) == 0: # first time run on certain tag and freq
+    if len(subjectNhSession.lastCodes) == 0: # first time run on certain tag and freq
       for i in range(0, len(codes)):
         # https://stackoverflow.com/questions/64527464/clickable-link-inside-message-discord-py
-        [tags, thumbnail, pages] = await getTagsAndThumbnailAndPagesFromCode(codes[i])
+        [tags, specialCode, pages] = await getTagsAndSpecialCodeAndPagesFromCode(codes[i])
         embed = discord.Embed()
-        if "http" not in thumbnail:
-          await channel.send(f"can't load the thumbnail, thumbnail fed: {thumbnail}")
-        else:
+        thumbnail = f"https://t.nhentai.net/galleries/{specialCode}/cover.jpg"
+        try:
           embed.set_image(url=thumbnail)
+        except:
+          pass
         embed.description = f"{captions[i]}\n\nTags: •{' •'.join(tags)}\nPages: {pages}\n\n[#{codes[i]}](https://nhentai.net/g/{codes[i]})."
         await channel.send(embed=embed)
-      lastCodes = codes
-      newStoredCodes = StoredCodes(tag, freq, codes)
-      savedCodes.append(newStoredCodes)
+      subjectNhSession.lastCodes = codes
+      subjectNhSession.savedCodes = codes
 
     else: # in loop
       adaBeda = False
       codeBeda = []
       for i in range(0, len(codes)):
-        if codes[i] in lastCodes:
+        if codes[i] in subjectNhSession.lastCodes:
           pass
         else:
           codeBeda.append(codes[i])
           adaBeda = True
-          [tags, thumbnail, pages] = await getTagsAndThumbnailAndPagesFromCode(codes[i])
+          [tags, specialCode, pages] = await getTagsAndSpecialCodeAndPagesFromCode(codes[i])
           embed = discord.Embed()
-          print(thumbnail)
-          print("\n")
-          if "http" not in thumbnail:
-            await channel.send(f"can't load the thumbnail, thumbnail fed: {thumbnail}")
-          else:
+          thumbnail = f"https://t.nhentai.net/galleries/{specialCode}/cover.jpg"
+          try:
             embed.set_image(url=thumbnail)
+          except:
+            pass
           embed.description = f"{captions[i]}\n\nTags: •{' •'.join(tags)}\nPages: {pages}\n\n[#{codes[i]}](https://nhentai.net/g/{codes[i]})."
           await channel.send(embed=embed)
       if not adaBeda:
-        await channel.send("no new art in popular(main page)")
+        print("nope")
+        sys.stdout.flush()
+       # await channel.send("no new art in popular(main page)")
       else:
-        for savedCode in savedCodes:
-          if savedCode.tag == tag and savedCode.freq == freq:
-            for code in codeBeda:
-              savedCode.codes.append(code)
-            break
-        lastCodes = codes
-    savedCodes = json.dumps([savedCode.__dict__ for savedCode in savedCodes])
-    print(savedCodes)
+        for newCode in codeBeda:
+            subjectNhSession.savedCodes.append(newCode)
+        subjectNhSession.lastCodes = codes
+    nhSessions = json.dumps([nhSession.__dict__ for nhSession in nhSessions], indent=4)
+    print(nhSessions)
+    sys.stdout.flush()
     f.seek(0)
-    f.write(savedCodes)
+    f.write(nhSessions)
     f.truncate()
     f.close()
-    await asyncio.sleep(3600)
+    await asyncio.sleep(1200)
 
 
 # this is the scrap function 
@@ -323,6 +327,7 @@ async def nhScraper(subjectLink: str, additionalSelector: str, amount: int) -> l
 
     #print(thumbnailURL)
     print(code)
+    sys.stdout.flush()
     codes.append(code)
     #thumbnails.append(thumbnailURL)
     captions.append(caption)
@@ -331,7 +336,7 @@ async def nhScraper(subjectLink: str, additionalSelector: str, amount: int) -> l
   return [codes, captions]
 
 
-async def getTagsAndThumbnailAndPagesFromCode(code: str = "177013") -> list[list[str], str, str]:
+async def getTagsAndSpecialCodeAndPagesFromCode(code: str = "177013") -> list[list[str], str, str]:
   """return tags, thumbail url and pages count given code """
   browser = await launch(ignoreHTTPSErrors = True, headless = True, args=["--no-sandbox"])
   page = await browser.newPage()
@@ -348,6 +353,12 @@ async def getTagsAndThumbnailAndPagesFromCode(code: str = "177013") -> list[list
   anchorPages = await page.querySelectorAll("a.tag")
   anchorPages = anchorPages[-1]
   pages =  await page.evaluate(""" (ele) => ele.querySelector("span.name").innerText """, anchorPages)
+  # https://t.nhentai.net/galleries/2012274/cover.jpg
   await page.close()
   await browser.close()
-  return [tags, thumbnailURL, pages]
+  return [tags, thumbnailURL[32:39], pages]
+
+async def bacaHaram(ctx, code):
+  [_, specialCode, pages] = await getTagsAndSpecialCodeAndPagesFromCode(code)
+  for i in range(int(pages)):
+    await ctx.send(f"https://i.nhentai.net/galleries/{specialCode}/{i + 1}.jpg")
